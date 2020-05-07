f9c1d8d1d3a4ebb2bd9d9a7ec9c184cdc8390ee8
gregorybchris <gregorybchris@gmail.com>
2020-05-07 00:19:27 -0700

    Add TrueSkill charts

--- generals_sdk/visualization/charts.py
+++ generals_sdk/visualization/charts.py
@@ -2,6 +2,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from trueskill import Rating, rate
+
 
 sns.set()
 sns.set_style('white')
@@ -32,6 +34,35 @@ def get_cumulative_ranks_df(games, rolling_average_n=None):
 def plot_cumulative_ranks(games, rolling_average_n=None):
     df = get_cumulative_ranks_df(games, rolling_average_n=rolling_average_n)
     sns.lineplot(x='Game Number', y='Average Normalized Rank', hue='Player', data=df)
+    plt.show()
+
+
+def get_trueskill_df(games):
+    df_results = []
+    trueskill_ratings = dict()
+    for game_number, game in enumerate(games):
+        for player in game.ranking:
+            if player not in trueskill_ratings:
+                trueskill_ratings[player] = Rating()
+        team_ratings = [[trueskill_ratings[player]] for player in game.ranking]
+        team_ranks = list(range(len(game.ranking)))
+        updated_team_ratings = rate(team_ratings, ranks=team_ranks)
+        updated_ratings = dict(zip(game.ranking, [r[0] for r in updated_team_ratings]))
+        trueskill_ratings.update(updated_ratings)
+
+        for player, trueskill_rating in trueskill_ratings.items():
+            df_result = {
+                'Game Number': game_number,
+                'Player': player,
+                'TrueSkill': trueskill_rating.mu,
+            }
+            df_results.append(df_result)
+    return pd.DataFrame(df_results)
+
+
+def plot_trueskill(games):
+    df = get_trueskill_df(games)
+    sns.lineplot(x='Game Number', y='TrueSkill', hue='Player', data=df)
     plt.show()
 
 
--- generals_sdk/visualization/stats.py
+++ generals_sdk/visualization/stats.py
@@ -1,3 +1,6 @@
+from trueskill import Rating, rate
+
+
 def _print_sorted_dict(d, invert=False, title=None):
     if title is not None:
         print('\n' + title)
@@ -5,6 +8,21 @@ def _print_sorted_dict(d, invert=False, title=None):
     sorted_d = sorted(list(d.items()), key=lambda p: multiplier * p[1])
     for k, v in sorted_d:
         print(f"  {k}: {v}")
+
+
+def _get_trueskill(games):
+    trueskill_ratings = dict()
+    for game_number, game in enumerate(games):
+        for player in game.ranking:
+            if player not in trueskill_ratings:
+                trueskill_ratings[player] = Rating()
+        team_ratings = [[trueskill_ratings[player]] for player in game.ranking]
+        team_ranks = list(range(len(game.ranking)))
+        updated_team_ratings = rate(team_ratings, ranks=team_ranks)
+        updated_ratings = dict(zip(game.ranking, [r[0] for r in updated_team_ratings]))
+        trueskill_ratings.update(updated_ratings)
+    ratings = {player: rating.mu for player, rating in trueskill_ratings.items()}
+    return ratings
 
 
 def print_stats(games):
@@ -23,3 +41,6 @@ def print_stats(games):
 
     fprw = {name: (sum([1 if rank == 0 else 0 for rank in ranks]) / len(ranks)) for name, ranks in ranking_map.items()}
     _print_sorted_dict(fprw, invert=True, title='Average First Place Win Rate')
+
+    trueskill = _get_trueskill(games)
+    _print_sorted_dict(trueskill, title='TrueSkill Ratings')
